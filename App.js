1bab13fc01a90992f3263a5fbdd1e87a5c439c3c
alexisparker01 <ap7@wellesley.edu>
2021-11-27 04:11:03 -0500

    login signup page base

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import Inputs from './components/loginsignup';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.appTitle}>
+        Tellesley
+      </Text>
+      <Inputs />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  appTitle: {
+     margin: 24,
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
 });
