2eecaee9eb3e955d8ac9886dbf23ab60b8a2ece3
kateamacvicar <80081116+kateamacvicar@users.noreply.github.com>
2021-11-30 21:38:25 -0500

    more changes to log in

--- components/newLogin.js
+++ components/newLogin.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { SafeAreaView, TextInput, Button } from 'react-native';
+import { SafeAreaView, View, TextInput, Button } from 'react-native';
 import { Card } from 'react-native-paper';
+import { loginStyle } from './loginStyle';
  
 export const LoginScreen = () => {
     return (
-        <SafeAreaView> 
-            <Card>
-            <Card.Title title = "Tellesley"></Card.Title>
-            <Card.Content>
-                <TextInput placeholder="Email" keyboardType="email-address"></TextInput>
-                <TextInput placeholder= "Password" secureTextEntry = {true} ></TextInput>
-                <Button>Login</Button>
-                <Button>Register</Button>
-            </Card.Content>
-            </Card>
+        <SafeAreaView style = {loginStyle.content}>
+            <View style = {loginStyle.view}>
+                <Card>
+                    {/* <Card.Title title = "Tellesley" titleStyle = {loginStyle.cardTitle}></Card.Title> */}
+                    <Card.Content>
+                        <TextInput placeholder="Email" keyboardType="email-address" style = {loginStyle.textFields}></TextInput>
+                        <TextInput placeholder= "Password" secureTextEntry = {true} style = {loginStyle.textFields}></TextInput>
+                    </Card.Content>
+                </Card>
+            </View>
+            <Button title = "Login"></Button>
+             <Button title = "Sign up"></Button>
         </SafeAreaView>
-
     )
 }
