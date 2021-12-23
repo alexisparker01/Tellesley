81648f7f6a6f30415c6da48739cef4dc8be3e8ab
alexisparker01 <ap7@wellesley.edu>
2021-12-23 09:45:42 -0600

    catergory filtering fixed

--- components/FeedMain.js
+++ components/FeedMain.js
@@ -55,16 +55,11 @@ const MessageItem = props => {
   return (
   <View style={styles.postContainer}>
     <Text style={styles.messageDateTime}>{formatDateTime(props.message.date)}</Text>
-<<<<<<< Updated upstream
-    <Text style={styles.messageAuthor}>{props.FName} {props.LName}</Text>
-    <Text style={styles.messagePost}>{props.message.post}</Text>
-    <TouchableOpacity mode = "contained" style = {styles.buttons}> <Text style = {styles.buttonText}> Delete </Text></TouchableOpacity>
-=======
+    <Text style={styles.messageCatergory}>{props.message.category}</Text>
     <Text style={styles.messageAuthor}>{props.message.user}</Text>
     <Text style={styles.messageContent}>{props.message.post}</Text>
-    <TouchableOpacity><Button style={styles.delButton}>Delete</Button></TouchableOpacity>
+    <TouchableOpacity><Button style={styles.buttons} title='Delete'></Button></TouchableOpacity>
 
->>>>>>> Stashed changes
   </View> 
 ); 
 }
@@ -74,7 +69,8 @@ export const Feed = ({navigation}) => {
 
   // State for chat channels and messages
   //const [category,setCategory] = React.useState(loggedInProps.categories);
-  const [selectedCategory, setSelectedCategory] = React.useState('Classes');
+  const [selectedCategory, setSelectedCategory] = React.useState('All');
+  console.log("the current catergory is: " + selectedCategory);
   const [selectedMessages, setSelectedMessages] = React.useState([]);
   const [textInputValue, setTextInputValue] = useState('');
   //const [localMessageDB, setLocalMessageDB] = useState(testMessages.map( addTimestamp ));
@@ -84,7 +80,7 @@ export const Feed = ({navigation}) => {
     return {...message, timestamp:message.date.getTime()}
   } 
 
-
+ 
 useEffect(
   () => { 
     getMessagesForCategory(selectedCategory); 
@@ -116,19 +112,24 @@ async function firebaseGetAllMessages(){
   setSelectedMessages(messages);
 }
 
+
+
+
 async function firebaseGetMessagesForCategory(cat) {
-  const q = query(collection(loggedInProps.db, 'messages'), where('category', '===', cat));
+  const q = query(collection(loggedInProps.db, 'messages'), where('category', '==', cat));
   const querySnapshot = await getDocs(q);
   let messages = []; 
   querySnapshot.forEach(doc => {
       messages.push(docToMessage(doc));
+      console.log("this is the doc in cat firebase messages " + doc);
   });
   setSelectedMessages(messages);
 }
 
 async function getMessagesForCategory(cat) {
-  if (cat !== 'All') {
+  if (cat != 'All') {
     firebaseGetMessagesForCategory(cat);
+
 }else{
     firebaseGetAllMessages();
 }
@@ -283,6 +284,13 @@ messageAuthor: {
   color:'rgb(8,58,129)',
 },
 messageContent: {
+  paddingLeft: 10,
+  paddingBottom: 3,
+  padding:5,
+  fontSize: 15,
+  color:'black',
+},
+messageCatergory: {
   paddingLeft: 10,
   paddingBottom: 3,
   padding:5,
